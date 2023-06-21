712f86e070393c192bb73305a029d78c097f4ea0
Maeve <mariya.kiryakulova@gmail.com>
2023-06-21 12:39:11 +0100

    link-pages-together

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,1 +1,12 @@
+import { Link } from "react-router-dom";
+import { navItems } from "../../utils/navItems";
 
+const Nav = () => {
+    return (
+        <div>
+            {navItems.map(({id, text, href}) => <Link to={href} key={id}>{text}</Link>)}
+        </div>
+    )
+}
+
+export default Nav
