36fc20209773887a9bd6d0ef00061d25cbfb3056
Maeve <mariya.kiryakulova@gmail.com>
2023-06-21 12:39:59 +0100

    add-router-to-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Movie from "./pages/Movie/Movie";
+import Favourite from "./pages/Favourite/Favourite";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+        <Route index element={<Navigate to="home"/>}/>
+        <Route path="home" element={<Home/>}/>
+        <Route path="movie" element={<Movie/>}/>
+        <Route path="favourite" element={<Favourite/>}/>
+      </Route>
+    </Routes>
   );
 }
 
