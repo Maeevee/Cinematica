2ece6071f8b94e670fe68bf99a5eb3f16e845146
Maeve <mariya.kiryakulova@gmail.com>
2023-06-24 22:10:33 +0100

    fetches-movies

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
+import { getPopular } from '../../servises/servises'
+import { IPopular } from '../../utils/interfaces';
+
 
 const Home = () => {
+
+  const [films, setFilms] = useState<[]|IPopular[]>([]);
+
+  useEffect( () => {
+    getPopular().then(date => setFilms(date)).catch(error => console.log(error))
+  }, [])
+
+  console.log(films);
+  
   return (
     <div>
-      <p>Home</p>
+      
     </div>
   )
 }
