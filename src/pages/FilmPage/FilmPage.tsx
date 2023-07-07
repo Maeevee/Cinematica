43d2761f9f27a2c81579585b6db6ba6d64d45e2c
Maeve <mariya.kiryakulova@gmail.com>
2023-07-08 00:22:29 +0100

    add-movie-information-structer

--- src/pages/FilmPage/FilmPage.tsx
+++ src/pages/FilmPage/FilmPage.tsx
@@ -49,17 +49,19 @@ const FilmPage = () => {
 
     return (
         <>
-        <div>
-            {film && (
-                <div>
-                    <img alt={film.title} src={`https://image.tmdb.org/t/p/w300/${film.backdrop_path}`}/>
-                    
-                    <div>
-                        <h2>{film.title}</h2>
-                        <div>Ganres:{film.genres?.map (({id, name}) => <p key={id}>{name}</p>)}</div>
-                        <p>{film.overview}</p>
+            <div className={`p-5 w-full ${styles.text}`}>
+                {film && (
+                    <div className={`flex mt-20 mb-10`}>
+                            <img className={`rounded-xl mr-10`} alt={film.title} src={`https://image.tmdb.org/t/p/w300/${film.poster_path}`}/>
                         <div>
+                            <h2 className={`${styles.title}`}>{film.title}</h2>
                             <ul className={`rounded-3xl`}>
+                                <li className={`rounded-t-xl ${styles.li} ${styles.liOdd}`}><span className={`font-bold`}>Year:</span> {year}</li>
+                                <li className={`${styles.li} ${styles.liEven}`}><span className={`font-bold`}>Ganres:</span>{film.genres?.map (({id, name}, index) => <span key={id}> {name}{index !== film.genres.length - 1 && ','}</span>)}</li>
+                                <li className={`${styles.li} ${styles.liOdd}`}><span className={`font-bold`}>Duration:</span> {film.runtime} min</li>
+                                <li className={`${styles.li} ${styles.liEven}`}><span className={`font-bold`}>Countrie:</span> {film.production_countries?.map (({id, name}, index) => <span key={id}> {name}{index !== film.production_countries.length - 1 && ','}</span>)}</li>
+                                <li className={`${styles.li} ${styles.liOdd}`}><span className={`font-bold`}>Language:</span> {film.spoken_languages?.map (({id, english_name}, index) => <span key={id}> {english_name}{index !== film.genres.length - 1 && ','}</span>)}</li>
+                                <li className={`${styles.li} ${styles.liEven}`}><span className={`font-bold`}>Synopsis:</span> {film.overview}</li>
                                 <li className={`rounded-b-xl ${styles.li} ${styles.liOdd} flex`}>{generateStars(raiting)}</li>
                             </ul>
                         </div>
