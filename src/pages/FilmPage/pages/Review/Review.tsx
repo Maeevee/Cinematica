2d7803c60f96b41c6154120838bf3cbc254eaeee
Maeve <mariya.kiryakulova@gmail.com>
2023-07-09 23:14:28 +0100

    add-page-structure

--- src/pages/FilmPage/pages/Review/Review.tsx
+++ src/pages/FilmPage/pages/Review/Review.tsx
@@ -37,9 +37,22 @@ const Review = () => {
 
 
   return (
-    <div>
-      
-    </div>
+    <ul className={`p-5`}>
+      {reviews.map((review, rating) =>
+      <li className={`border-b-2 border-cyan-700/30`} key={rating}>
+        <img className={`rounded-full w-16 h-16`} src={review.author_details?.avatar_path?.startsWith("/https://secure.gravatar.com/avatar/") ? review.author_details.avatar_path.slice(1) : `https://image.tmdb.org/t/p/w300/${review.author_details.avatar_path}`} alt={review.author_details?.username} onError={(e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+              (e.target as HTMLImageElement).src =
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbWMHsOFSpe0SQ9au-xUAPizVVualR_yqqMg&usqp=CAU';
+              (e.target as HTMLImageElement).alt = 'placeholder';
+            }} />
+        <div>
+          <h3>{review.author_details.username}</h3>
+          <p> {new Date(review.created_at).toLocaleDateString()}</p>
+        </div>
+        {/* <p>{generateStars(rating)}</p> */}
+        <p>{review.content}</p>
+      </li>)}
+    </ul>
   )
 }
 
