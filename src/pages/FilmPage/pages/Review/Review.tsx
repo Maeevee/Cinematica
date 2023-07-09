a164033b67a9cfc64dac5e0809aef912013ed7a7
Maeve <mariya.kiryakulova@gmail.com>
2023-07-09 23:13:56 +0100

    add-rating-system

--- src/pages/FilmPage/pages/Review/Review.tsx
+++ src/pages/FilmPage/pages/Review/Review.tsx
@@ -17,6 +17,25 @@ const Review = () => {
     return <div>Loading...</div>;
   }
 
+//   const rating = (reviews[0] as IReview).author_details.rating as number;
+//   const generateStars = (rating: number) => {
+//     const stars = [];
+//     const fiveStar = rating / 2;
+//     console.log(fiveStar);
+    
+//     for (let i = 0; i < 5; i++) {
+//         if (i < fiveStar) {
+//             stars.push(<span key={i} className={`text-yellow-400 text-2xl`}>★</span>)
+//         } else {
+//             stars.push(<span key={i} className={`text-gray-400 text-2xl`}>★</span>)
+//         }
+//     }
+//     return stars;
+// }
+
+
+
+
   return (
     <div>
       
