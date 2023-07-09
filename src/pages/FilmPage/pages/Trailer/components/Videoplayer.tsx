4e10173b81ef75dc0d68c0171ee574ab112695d3
Maeve <mariya.kiryakulova@gmail.com>
2023-07-10 00:05:18 +0100

    Update Videoplayer.tsx

--- src/pages/FilmPage/pages/Trailer/components/Videoplayer.tsx
+++ src/pages/FilmPage/pages/Trailer/components/Videoplayer.tsx
@@ -1,19 +1,22 @@
-// import Iframe from 'react-iframe';
+import Iframe from 'react-iframe';
 
-// export const Videoplayer = ({ data }) => {
-//     return (
-//       <ul>
-//         {data.map(({ id, key, name }) => (
-//           <li loading="lazy" key={id}>
-//             <Iframe
-//               title={name}
-//               url={`https://www.youtube.com/embed/${key}?rel=0&showinfo=0&autoplay=1`}
-//               width="100%"
-//               height="320px"
-//               overflow="cover"
-//             />
-//           </li>
-//         ))}
-//       </ul>
-//     );
-//   };
+export const Videoplayer = ({ data }: { data: Array<any> }) => {
+  return (
+    <ul>
+      {data.map(({ id, key, name }: { id: string; key: string; name: string }) => (
+        <li key={id}>
+          <div style={{ overflow: 'hidden' }}>
+            <Iframe
+              title={name}
+              url={`https://www.youtube.com/embed/${key}?rel=0&showinfo=0&autoplay=1`}
+              width="100%"
+              height="320px"
+            />
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+
