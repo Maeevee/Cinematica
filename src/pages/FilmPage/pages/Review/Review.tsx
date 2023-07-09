2f33767dd1b3b568c9eaf261015fcadcd8314da9
Maeve <mariya.kiryakulova@gmail.com>
2023-07-09 23:12:08 +0100

    add-hooks

--- src/pages/FilmPage/pages/Review/Review.tsx
+++ src/pages/FilmPage/pages/Review/Review.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
 const Review = () => {
+
+  const {filmId} = useParams();
+  const [reviews, setReviews] = useState<[]|IReview[]>([]);
+
+  useEffect ( ()  =>{
+    getReview(filmId as string).then (setReviews).catch(console.log)
+  }, [filmId])
+
   return (
     <div>
       
