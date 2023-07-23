116ab01217855a336a3049af6740ef821f6bae29
Maeve <mariya.kiryakulova@gmail.com>
2023-07-23 23:08:39 +0100

    Update ListItem.tsx

--- src/pages/Home/components/ListItem/ListItem.tsx
+++ src/pages/Home/components/ListItem/ListItem.tsx
@@ -1,10 +1,21 @@
+import { Link, useLocation } from 'react-router-dom'
 import styles from './ListItem.module.scss'
+import { IPopular } from '../../../../utils/interfaces'
 
-const ListItem = () => {
+interface Props{film: IPopular}
+
+const ListItem = ({film}: Props) => {
+
+    const location = useLocation();
+    console.log(location);
+    
     return (
-        <div>
-        
-        </div>
+        <li>
+            <Link to={`/movie/${film.id}`} state={{from:location}}>
+                <img alt={film.title} src={`https://image.tmdb.org/t/p/w300/${film.poster_path}`}/>
+                <h2>{film.title}</h2>
+            </Link>
+        </li>
     )
 }
 
