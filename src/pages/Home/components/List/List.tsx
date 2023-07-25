3a5778d58f88fc97b31b3127e004b0bd2d85a83f
Maeve <mariya.kiryakulova@gmail.com>
2023-07-25 18:27:55 +0100

    transfer-code-from-home-to-list

--- src/pages/Home/components/List/List.tsx
+++ src/pages/Home/components/List/List.tsx
@@ -1,9 +1,25 @@
-import styles from './List.module.scss'
+
+import ListItem from '../ListItem/ListItem';
+import { useState, useEffect } from 'react';
+import { getPopular } from '../../../../servises/servises';
+import styles from './List.module.scss';
+import { IPopular } from '../../../../utils/interfaces';
 
 const List = () => {
+
+    const [films, setFilms] = useState<[]|IPopular[]>([]);
+
+    useEffect( () => {
+        getPopular().then(date => setFilms(date)).catch(error => console.log(error))
+    }, [])
+
     return (
-        <div>
-        
+        <div className={`p-5 w-full`}>
+            <ul className={`mt-20 flex flex-wrap justify-center`}>
+            {films.map((film) =>
+                <ListItem key={film.id} film={film}/>
+            )}
+            </ul>
         </div>
     )
 }
