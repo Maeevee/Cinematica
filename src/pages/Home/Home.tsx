993e3497b4e63de1f609716768b1e366831c6138
Maeve <mariya.kiryakulova@gmail.com>
2023-08-03 16:26:51 +0100

    add-pagination-component-to-home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,19 +2,27 @@ import List from './components/List/List';
 import { IPopular } from '../../utils/interfaces';
 import { useState, useEffect } from 'react';
 import { getPopular } from '../../servises/servises';
+import { useSearchParams } from 'react-router-dom';
+import PaginatedItems from '../../components/Pagination/Pagination';
 
 
 const Home = () => {
 
   const [films, setFilms] = useState<[]|IPopular[]>([]);
 
+  const [totalPages, setTotalPage] = useState(0)
+
   useEffect( () => {
-    getPopular().then(date => setFilms(date)).catch(error => console.log(error))
+    getPopular().then(date => {setFilms(date.results); setTotalPage(date.total_pages)}).catch(error => console.log(error))
 }, [])
+
+const [searchParems, setSearchParems] = useSearchParams();
+const pageNumber = Number(searchParems.get('page') ?? 1)
 
   return (
     <div>
       <List movieList={films}/>
+      <PaginatedItems setPageNumber={setSearchParems} totalPages={totalPages} currentPage={pageNumber - 1} />
     </div>
   )
 }
