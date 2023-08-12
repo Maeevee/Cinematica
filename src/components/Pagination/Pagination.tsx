12a77ae09e7ba93fcc6494780a471bc602172914
Maeve <mariya.kiryakulova@gmail.com>
2023-08-12 23:38:49 +0100

    fix-pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -10,27 +10,27 @@ function PaginatedItems({ setPageNumber, totalPages, currentPage }:Props) {
 
     return (
         <>
-          <ReactPaginate
-            pageCount={totalPages}
-            forcePage={currentPage}
-            onPageChange={handlePageClick}
-            breakLabel="..."
-            nextLabel="next >"
-            previousLabel="< previous"
-            renderOnZeroPageCount={null}
-            pageRangeDisplayed={2}
-            marginPagesDisplayed={4}
-            containerClassName="pagination"
-            activeClassName="active"
-            breakClassName="item"
-            breakLinkClassName="link"
-            pageClassName="item"
-            pageLinkClassName="link"
-            previousClassName="item"
-            previousLinkClassName="link"
-            nextClassName="item"
-            nextLinkClassName="link"
-          />
+            <ReactPaginate className={styles.pagination}
+                pageCount={totalPages}
+                forcePage={currentPage}
+                onPageChange={handlePageClick}
+                breakLabel="..."
+                nextLabel="next >"
+                previousLabel="< previous"
+                renderOnZeroPageCount={null}
+                pageRangeDisplayed={pageRange}
+                marginPagesDisplayed={marginPages}
+                containerClassName="pagination"
+                activeClassName={styles.active}
+                breakClassName={styles.item}
+                breakLinkClassName={styles.link}
+                pageClassName={styles.item}
+                pageLinkClassName={styles.link}
+                previousClassName={styles.previous}
+                previousLinkClassName={styles.link}
+                nextClassName={styles.next}
+                nextLinkClassName={styles.link}
+            />
         </>
       );
     }
