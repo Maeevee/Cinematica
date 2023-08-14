80a56ec0e56f6ae4ef549983c8c9575404b3c7fc
Maeve <mariya.kiryakulova@gmail.com>
2023-08-14 16:09:08 +0100

    add-pagination-to-favourite

--- src/pages/Favourite/Favourite.tsx
+++ src/pages/Favourite/Favourite.tsx
@@ -1,6 +1,8 @@
 import { useState} from 'react'
 import { IPopular } from '../../utils/interfaces';
-import ListItem from '../Home/components/ListItem/ListItem';
+import List from '../Home/components/List/List';
+import PaginatedItems from '../../components/Pagination/Pagination';
+import { useSearchParams } from 'react-router-dom';
 
 const Favourite = () => {
 
@@ -10,13 +12,13 @@ const Favourite = () => {
     return data;
   });
 
+const [searchParems, setSearchParems] = useSearchParams();
+const pageNumber = Number(searchParems.get('page') ?? 1)
+
   return (
     <div>
-      <ul>
-        {favourite.map((film) =>
-          <ListItem key={film.id} film={film}/>
-        )}
-      </ul>
+          <List movieList={favourite}/>
+      <PaginatedItems setPageNumber={setSearchParems} totalPages={Math.ceil(favourite.length/20)} currentPage={pageNumber - 1} />
     </div>
   )
 }
