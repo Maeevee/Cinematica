3206a2881a2bde9defa680411ed8236c203f8080
Maeve <mariya.kiryakulova@gmail.com>
2023-08-14 15:41:13 +0100

    delete-tailwind-and-add-mui-grid-to-list

--- src/pages/Home/components/List/List.tsx
+++ src/pages/Home/components/List/List.tsx
@@ -10,12 +10,12 @@ interface Props{movieList: IPopular[]|[]}
 const List = ({movieList} : Props) => {
 
     return (
-        <div className={`p-5 w-full`}>
-            <ul className={`mt-20 flex flex-wrap justify-center`}>
+        <div className={styles.container}>
+            <Grid container spacing={{ xs: 2, sm: 2, md: 3 , lg: 5}} columns={{ xs: 12, sm: 12, md: 12, lg: 10 }}>
             {movieList.map((film) =>
                 <ListItem key={film.id} film={film}/>
             )}
-            </ul>
+            </Grid>
         </div>
     )
 }
