844a9288a637a98e37fbdeb396299c052502d6ad
Maeve <mariya.kiryakulova@gmail.com>
2023-08-14 15:50:24 +0100

    delete-commented-code-and-logs

--- src/pages/Home/components/ListItem/ListItem.tsx
+++ src/pages/Home/components/ListItem/ListItem.tsx
@@ -8,34 +8,6 @@ interface Props{film: IPopular}
 const ListItem = ({film}: Props) => {
 
     const location = useLocation();
-    console.log(location);
-    console.log(film);
-    
-
-    // const [hovered, setHovered] = useState(false);
-
-    // const handleMouseEnter = () => {
-    //     setHovered(true);
-    // };
-    
-    // const handleMouseLeave = () => {
-    //     setHovered(false);
-    // };
-    
-    // const imagePath = hovered ? film.backdrop_path : film.poster_path;
-    
-    // const containerStyle: React.CSSProperties = {
-    //     backgroundImage: `url(https://image.tmdb.org/t/p/w300/${film.backdrop_path})`,
-    //     width: hovered ? '200%' : '100%',
-    //     height: hovered ? '50%' : '100%',
-    //     transition: 'width 0.5s ease',
-    //     borderRadius: '20px',
-    //   };
-    
-    //   const imageStyle: React.CSSProperties = {
-
-    //   };
-
 
     const releaseDate = new Date(film?.release_date as string);
     const year = new Date(releaseDate).getFullYear();
