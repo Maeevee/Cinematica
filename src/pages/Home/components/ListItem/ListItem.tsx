405fdf9d4b6bf418ca997180f328e306a1bd5e0f
Maeve <mariya.kiryakulova@gmail.com>
2023-08-14 15:52:09 +0100

    fix-listitem-structure

--- src/pages/Home/components/ListItem/ListItem.tsx
+++ src/pages/Home/components/ListItem/ListItem.tsx
@@ -1,7 +1,10 @@
 import { Link, useLocation } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import styles from './ListItem.module.css'
 import { IPopular } from '../../../../utils/interfaces'
 import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { Grid } from '@mui/material'
 
 interface Props{film: IPopular}
 
@@ -14,24 +17,20 @@ const ListItem = ({film}: Props) => {
     
     
     return (
-        <li className={`p-5 w-64 m-0 `}>
-            <Link to={`/movie/${film.id}`} state={{from:location}}>
-                <div className={styles.poster}>
-                    <img alt={film.title} src={`https://image.tmdb.org/t/p/w300/${film.poster_path}`} className={`rounded-xl`}/>
-                    <div className={styles.posterInfo}>
-                        <h2 className={`text-white text-2xl font-bold`}>{film.title}</h2>
-                        <p>{year}</p>
-                        <p>{film.vote_average}</p>
-                        <button>Watch</button>
-                        
-                        
-
-
+        <Grid item xs={6} sm={4} md={3} lg={2}>
+                <NavLink to={`/movie/${film.id}`} state={{from:location}}>
+                    <div className={styles.poster} ref={containerRef} style={{ height: `${height}px` }}>
+                        <img alt={film.title} src={`https://image.tmdb.org/t/p/w300/${film.poster_path}`} className={styles.posterImg} style={{ width: '100%', height: `${height}px` }}
+            onLoad={updateHeight}/>
+                        <div className={styles.posterInfo}>
+                            <h2>{film.title}</h2>
+                            <p>{year}</p>
+                            <p>{film.vote_average}</p>
+                            <button>Watch</button>
+                        </div>
                     </div>
-                </div>
-                {/* <h2>{film.title}</h2> */}
-            </Link>
-        </li>
+                </NavLink>
+        </Grid>
     )
 }
 
