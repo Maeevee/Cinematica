193105d620057024d6ea8936c0fe04d045db006b
Maeve <mariya.kiryakulova@gmail.com>
2023-08-24 00:19:10 +0100

    fix-film-page

--- src/pages/FilmPage/FilmPage.tsx
+++ src/pages/FilmPage/FilmPage.tsx
@@ -6,47 +6,21 @@ import { getById } from '../../servises/servises';
 import { IFilm } from '../../utils/interfaces';
 import { navFilm } from '../../utils/navItems';
 import styles from "./FilmPage.module.css";
-import { IPopular } from '../../utils/interfaces';
 import { Link } from 'react-router-dom';
+import Rating from '../../components/Rating/Rating';
+import SaveDeleteButton from '../../components/SaveDeleteButton/SaveDeleteButton';
 
 
 const FilmPage = () => {
 
     const {filmId} = useParams();
     const [film, setFilm] = useState<null|IFilm>();
-    
-    const [buttonVariant, setButtonVariant] = useState(() => {
-        const storage = localStorage.getItem("favourite");
-        const data : IPopular[]|[] = JSON.parse (storage as string) ?? [];
-        const isInStorage = data.find(item => item?.id === film?.id) 
-        if (!isInStorage) {
-            return false
-        } 
-        if (isInStorage) {
-            return true
-        }
-    })
 
     const location = useLocation();
     
     const backLink = location?.state?.from ?? "/"
 
     const backLocation = useRef(backLink)
-
-    const handleSave = () => {
-        const storage = localStorage.getItem("favourite");
-        const data : IPopular[]|[] = JSON.parse (storage as string) ?? [];
-        const isInStorage = data.find(item => item?.id === film?.id) 
-        if (!isInStorage) {
-            localStorage.setItem("favourite", JSON.stringify([...data, film]))
-            setButtonVariant(true)
-        } 
-        if (isInStorage) {
-            const newData = data.filter(item => item?.id !== film?.id)
-            localStorage.setItem("favourite", JSON.stringify([...newData]))
-            setButtonVariant(false)
-        }
-    }
     
     useEffect( () => {
         getById(filmId as string).then (response => {
@@ -55,38 +29,10 @@ const FilmPage = () => {
         }).catch(error => console.log(error));
     }, [filmId])
 
-    useEffect( () => {
-        const storage = localStorage.getItem("favourite");
-        const data : IPopular[]|[] = JSON.parse (storage as string) ?? [];
-        const isInStorage = data.find(item => item?.id === film?.id) 
-        if (!isInStorage) {
-            setButtonVariant(false)
-        } 
-        if (isInStorage) {
-            setButtonVariant(true)
-        }
-    }, [film?.id])
-
     const releaseDate = new Date(film?.release_date as string);
     const year = new Date(releaseDate).getFullYear();
 
     const raiting = film?.vote_average as number;
-
-    const generateStars = (raiting: number) => {
-        const stars = [];
-        const fiveStar = raiting / 2;
-        
-        const raitingRound = Math.round(fiveStar);
-        
-        for (let i = 0; i < 5; i++) {
-            if (i < raitingRound) {
-                stars.push(<span key={i} className={`text-yellow-400 text-4xl`}>★</span>)
-            } else {
-                stars.push(<span key={i} className={`text-gray-400 text-4xl`}>★</span>)
-            }
-        }
-        return stars;
-    }
 
     // Tabs
     const [selectedTab, setSelectedTab] = React.useState(1);
@@ -97,37 +43,71 @@ const FilmPage = () => {
     };
 
     return (
-        <>
-            <div className={`p-5 w-full ${styles.text}`}>
-                {film && (
-                    <div className={`flex mt-20 mb-10`}>
-                            <img className={`rounded-xl mr-10`} alt={film.title} src={`https://image.tmdb.org/t/p/w300/${film.poster_path}`}/>
-                        <div>
-                            <h2 className={`${styles.title}`}>{film.title}</h2>
-                            <ul className={styles.list}>
-                                <li className={`rounded-t-xl ${styles.li} ${styles.liOdd}`}><span className={`font-bold`}>Year:</span> {year}</li>
-                                <li className={`${styles.li} ${styles.liEven}`}><span className={`font-bold`}>Ganres:</span>{film.genres?.map (({id, name}, index) => <span key={id}> {name}{index !== film.genres.length - 1 && ','}</span>)}</li>
-                                <li className={`${styles.li} ${styles.liOdd}`}><span className={`font-bold`}>Duration:</span> {film.runtime} min</li>
-                                <li className={`${styles.li} ${styles.liEven}`}><span className={`font-bold`}>Countrie:</span> {film.production_countries?.map (({id, name}, index) => <span key={id}> {name}{index !== film.production_countries.length - 1 && ','}</span>)}</li>
-                                <li className={`${styles.li} ${styles.liOdd}`}><span className={`font-bold`}>Language:</span> {film.spoken_languages?.map (({id, english_name}, index) => <span key={id}> {english_name}{index !== film.spoken_languages.length - 1 && ','}</span>)}</li>
-                                <li className={`${styles.li} ${styles.liEven}`}><span className={`font-bold`}>Synopsis:</span> {film.overview}</li>
-                                <li className={`rounded-b-xl ${styles.li} ${styles.liOdd} flex`}>{generateStars(raiting)}</li>
-                            </ul>
-                        </div>
+        <div className={styles.filmPageContainer}>
+            <div className={styles.filmInfoContainer}>
+            {film && (
+                <div className={styles.flexContainer}>
+                    <div className={styles.imageContainer}>
+                        <img className={styles.roundedImg} alt={film.title} src={`https://image.tmdb.org/t/p/w300/${film.poster_path}`} />
+                        <div className={styles.gradientOverlay}></div>
+                        {film && <SaveDeleteButton film={film} className={styles.SaveDeleteButton}/>}
+                        <div className={styles.GoBackButtonContainer}><NavLink  to={backLocation.current}><svg className={styles.GoBackButton} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z"/></svg></NavLink></div>
                     </div>
-                )}
+                    <div className={styles.listContainer}>
+                        <h2 className={styles.title}>{film.title}</h2>
+                        <ul className={styles.list}>
+                            <li className={`${styles.roundedTopLi} ${styles.li}`}>
+                            <span>Year:</span> {year}
+                            </li>
+                            <li className={styles.li}>
+                            <span>Genres: </span>
+                            {film.genres?.map(({ id, name }, index) => (
+                                <span key={id}>
+                                {name}
+                                {index !== film.genres.length - 1 && ','}
+                                </span>
+                            ))}
+                            </li>
+                            <li className={styles.li}>
+                            <span>Duration:</span> {film.runtime} min
+                            </li>
+                            <li className={styles.li}>
+                            <span>Country: </span>
+                            {film.production_countries?.map(({ id, name }, index) => (
+                                <span key={id}>
+                                {name}
+                                {index !== film.production_countries.length - 1 && ','}
+                                </span>
+                            ))}
+                            </li>
+                            <li className={styles.li}>
+                            <span>Language: </span>
+                            {film.spoken_languages?.map(({ id, english_name }, index) => (
+                                <span key={id}>
+                                {english_name}
+                                {index !== film.spoken_languages.length - 1 && ','}
+                                </span>
+                            ))}
+                            </li>
+                            <li className={styles.li}>
+                            <span>Synopsis:</span> {film.overview}
+                            </li>
+                            <li className={`${styles.roundedBottomLi} ${styles.li} ${styles.flex}`}>
+                            <Rating rating={raiting} />
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            )}
             </div>
-            <button onClick={handleSave}>{buttonVariant? 'delete' : 'save'}</button>
-            {/* @ts-ignore */}
-            <div className={`mt-11`}><NavLink  to={backLocation.current}>Go back</NavLink></div>
-            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                <Tabs value={selectedTab} onChange={handleChange} variant="fullWidth" aria-label="nav tabs example" centered>
+            <Box className={styles.box} sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Tabs className={styles.tabs} value={selectedTab} onChange={handleChange} variant="fullWidth" aria-label="nav tabs example" centered>
                     {navFilm.map(({id, text, href}) => (
-                        <Tab key={id} label={text} value={href} to={href} component={Link}/>))}
+                        <Tab className={styles.tab} key={id} label={text} value={href} to={href} component={Link}/>))}
                 </Tabs>
             </Box>
             <Outlet/>
-                    </>
+        </div>
     )
 };
 
