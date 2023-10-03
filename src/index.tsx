03e914ed6d3b83bc4597b2b6df2ae6d09acb403d
Mariia Kiriakulova <mariya.kiryakulova@gmail.com>
2023-10-03 22:12:10 +0100

    fix-base-styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -14,4 +14,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
