6dd651021b688b3cf1deef3b71cbedb269c32ca8
Mariia Kiriakulova <mariya.kiryakulova@gmail.com>
2023-10-03 22:13:07 +0100

    fix-film-page

--- src/pages/FilmPage/FilmPage.tsx
+++ src/pages/FilmPage/FilmPage.tsx
@@ -50,8 +50,9 @@ const FilmPage = () => {
             ? '3rem 3rem 0 0'
             : '3rem',
         backdropFilter: 'blur(20px)',
-        margin: '0 30px',
     };
+
+    const poster = window.innerWidth > 480 ? film?.poster_path : film?.backdrop_path
 
     return (
         <div className={styles.filmPageContainer}>
@@ -59,7 +60,7 @@ const FilmPage = () => {
             {film && (
                 <div className={styles.flexContainer}>
                     <div className={styles.imageContainer}>
-                        <img className={styles.roundedImg} alt={film.title} src={`https://image.tmdb.org/t/p/w300/${film.poster_path}`} />
+                        <img className={styles.roundedImg} alt={film.title} src={`https://image.tmdb.org/t/p/w300/${poster}`} />
                         <div className={styles.gradientOverlay}></div>
                         {film && <SaveDeleteButton film={film} className={styles.SaveDeleteButton}/>}
                         <div className={styles.GoBackButtonContainer}><NavLink  to={backLocation.current}><svg className={styles.GoBackButton} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z"/></svg></NavLink></div>
